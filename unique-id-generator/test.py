a50ff83faa66f80fed209950cb770e3442126819
hoangperry <prodai9573@gmail.com>
2022-12-05 17:36:09 +0700

    update readme

--- unique-id-generator/test.py
+++
@@ -1,6 +0,0 @@
-
-if __name__ == '__main__':
-    ip_list = ['1', '2', '3', '4', '5', '1', '2', '6', '7', '6', '1', '2', '8', '9', '3', '3']
-    # only 5-bits length limitation 32 in decimal
-    fake_hashtable = {}
-
