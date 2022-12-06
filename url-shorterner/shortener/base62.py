ea7251396a0d6254e473a6fd4d843ba7f4917c71
hoangperry <prodai9573@gmail.com>
2022-12-06 15:01:05 +0700

    init docker-compose

---
+++ url-shorterner/shortener/base62.py
@@ -0,0 +1,25 @@
+BASE_62_CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def encode(num, alphabet=BASE_62_CHARSET):
+    if num == 0:
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+
+def decode(string, alphabet=BASE_62_CHARSET):
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+    return num
--- url-shorterner/shortener/models.py
+++ url-shorterner/shortener/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class Shorten(models.Model):
     id = models.BigIntegerField(primary_key=True)
-    owner_id = models.BigIntegerField()
-    tag_name = models.TextField()
+    shorten = models.CharField(max_length=7)
+    origin = models.CharField(max_length=512)
+
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     updated_at = models.DateTimeField(null=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
