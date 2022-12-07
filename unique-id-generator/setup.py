e991299cab68bdf7ce519d25798ac77f19958233
hoangperry <prodai9573@gmail.com>
2022-12-07 14:34:47 +0700

    update version 0.1.0

--- unique-id-generator/setup.py
+++ unique-id-generator/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6'
+VERSION = '0.1.0'
 DESCRIPTION = 'Unique ID Generator'
 LONG_DESCRIPTION = 'An implementation for System Design Interview books'
 
--- unique-id-generator/unique_id_generator/id_gen.py
+++ unique-id-generator/unique_id_generator/id_gen.py
@@ -1,10 +1,16 @@
 import time
 import socket
 import requests
+from datetime import datetime
 
 
 HASH_SERVICE = 'http://127.0.0.1:8000/?value='
 EXTERNAL_API_SERVICE = 'https://api.ipify.org'
+
+
+# Limit of ID Generator is 2199023255552 millisecond from this date, around ~69 years
+dt_obj = datetime.strptime('7.12.2022 00:00:00,01', '%d.%m.%Y %H:%M:%S,%f')
+ANCHOR_TIME = int(dt_obj.timestamp() * 1000)
 
 
 class IdGen:
@@ -13,13 +19,13 @@ class IdGen:
         # Machine ID muse less than ~1000 (less than 10 bit)
         # Ideally the machine ID must split to 2 element is Cluster/Datacenter IDs and Machine/Service IDs
         self.machine_id = self.__get_machine_id() if machine_id is None else machine_id
-        self.last_time: int = 0
+        self.last_time: int = self.get_timestamp()
         self.sequence = 0
 
     @staticmethod
     def get_timestamp():
         # Improve here
-        return int((time.time() * 1000) % (10 ** 11))
+        return int(((time.time() * 1000)-ANCHOR_TIME) % (10 ** 11))
 
     @staticmethod
     def get_machine_external_ip():
@@ -27,7 +33,7 @@ class IdGen:
         return requests.get(EXTERNAL_API_SERVICE).content.decode('utf8')
 
     def set_timestamp(self, tt):
-        if tt == self.last_time:
+        if tt != self.last_time:
             self.last_time = tt
             self.sequence = 0
 
@@ -47,7 +53,7 @@ class IdGen:
 Test ID gen
 """
 if __name__ == '__main__':
-    gene = IdGen()
+    gene = IdGen(machine_id=123)
     print(gene.machine_id)
     list_id = list()
     for i in range(100000000):
